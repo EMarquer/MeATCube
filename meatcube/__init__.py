ca91a81fae2190271d04b1a0741e82f0c5fce2cb
Esteban Marquer <e.marquer1@gmail.com>
2023-08-01 16:46:55 +0200

    Add coverage and testing

--- meatcube/__init__.py
+++ meatcube/__init__.py
@@ -1,2 +1,1 @@
 from .meatcubecb import MeATCubeCB, MeATCubeCB as CB # use CB as an extra alias for MeATCubeCB
-from .plotting import *
--- meatcube/plotting/__init__.py
+++ meatcube/plotting/__init__.py
@@ -1,1 +1,4 @@
-from .energymap import *
+from .preprocessing import prepare_ax
+from .energymap import energymap
+from .cbscatter import plot_cb, plot_ref
+from .influencemap import influencemap
