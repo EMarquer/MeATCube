12150b7c6cdeff28f428e12d326593a8ebbacf47
Esteban Marquer <e.marquer1@gmail.com>
2024-11-08 14:11:54 +0100

    test: add basic tests for the EnergyKNN implementation

--- test/conftest.py
+++ test/conftest.py
@@ -2,13 +2,14 @@ import pytest
 from typing import Tuple, List
 import pandas as pd
 import numpy as np
+import numpy.linalg as npl
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
 # load meatcube
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-import meatcube2.meatcube_torch as mc
+import meatcube2.models.meatcube_torch as mc
 
 
 
@@ -53,32 +54,107 @@ def iris_num_split(iris_num) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.
 def iris_cb_train_size(iris_num_split) -> int:
     return iris_num_split[0].shape[0]
 
-@pytest.fixture(scope="module") # /!\ do not do modifications on the cbs
-def iris_str_cb(iris_str, iris_str_split) -> mc.MeATCubeCB:
-    X, y, y_values = iris_str
-    X_train, X_test, y_train, y_test = iris_str_split
-    return iris_cb(X_train, y_train, y_values)
-
-@pytest.fixture(scope="module") # /!\ do not do modifications on the cbs
-def iris_num_cb(iris_num, iris_num_split) -> mc.MeATCubeCB:
-    X, y, y_values = iris_num
-    X_train, X_test, y_train, y_test = iris_num_split
-    return iris_cb(X_train, y_train, y_values)
-
 def iris_split(X, y, y_values) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
     # stratified splitting of the data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=100, random_state=42, stratify=y)
     return X_train, X_test, y_train, y_test
 
-def iris_cb(X_train:pd.DataFrame, y_train:pd.DataFrame, y_values) -> mc.MeATCubeCB:
-    # create the CB
-    source_similarity = _source_similarity
-    outcome_similarity = _outcome_similarity
-    cb = mc.MeATCubeCB(source_similarity, outcome_similarity)
-    cb.fit(X_train.to_numpy(), y_train.to_numpy())#, y_values, )
-    return cb
+# def iris_cb(X_train:pd.DataFrame, y_train:pd.DataFrame, y_values) -> mc.MeATCubeCB:
+#     # create the CB
+#     source_similarity = _source_similarity
+#     outcome_similarity = _outcome_similarity
+#     cb = mc.MeATCubeCB(source_similarity, outcome_similarity)
+#     cb.fit(X_train.to_numpy(), y_train.to_numpy())#, y_values, )
+#     return cb
 
 
 # @pytest.fixture()
 # def test_cbs(iris_num_cb, iris_str_cb) -> List[mc.CB]:
 #     return [iris_num_cb, iris_str_cb]
+
+
+
+
+@pytest.fixture(scope="module")
+def manual_energy():
+    X_cb = np.array([[-1,0],[1,0]])
+    y_cb = np.array([1,0])
+
+    # with |CB|=2, 
+    # energy increase for CtCoat should be 
+    #   7.5 + 15/2 + 3*simS(X,\bluecircle) - 2*simS(X,\redsquare) + simS(\bluecircle,\redsquare)
+    #   7.5 + 15/2 + 3*simS(X,\bluecircle) - 2*simS(X,\redsquare) + simS(\bluecircle,\redsquare)
+    manual_energy_ = dict()
+
+    manual_energy_["CB X"] = X_cb
+    manual_energy_["CB y"] = y_cb
+
+    N = 10
+    np.random.seed(42)
+    X = np.random.rand(N,2)
+    y = np.random.randint(2,size=N)
+    manual_energy_["X"] = X
+    manual_energy_["y"] = y
+
+    # energy kNN
+    manual_energy_["EnergyKNN"] = dict()
+    EnergyKNN_energy_1nn = lambda x, y: 1-1*(
+        int(y == 1) if npl.norm(x - X_cb[0]) <= npl.norm(x - X_cb[1]) else int(y == 0)
+    )
+    EnergyKNN_energy_2nn = lambda x, y: 1-(1/2)*(
+        int(y == 1) + int(y == 0)
+    )
+    """
+    kNN(s_t) = argmax_{r ∈ R}(∑_{(s_i,r_i) ∈ CB} simS(s_i,s_t) ⋅ simR(r_i,r) )
+    EkNN(s_t, r) = 1 - (1/k) * ∑ {(s_i,r_i) ∈ CB} 1_{N_k}(s_i,s_t) ⋅ simR(r_i,r)
+
+    for CB: 
+    {
+       ([-1,0], 1),
+       ([1,0],  0)
+    }
+    energy of ([-1,0], 1) is: 
+    EkNN([-1,0], 1) = 1 - 1/k * ∑[
+        1_{N_k}([-1,0], [-1,0]) * simR(1, 1)
+        1_{N_k}([1,0], [-1,0]) * simR(0, 1)
+    ]
+    for k=1:
+    EkNN([-1,0], 1) = 1 - 1/1 * 1 = 0
+    EkNN([1,0],  0) = 1 - 1/1 * 1 = 0
+    EkNN([-1,0], 0) = 1 - 1/1 * 0 = 1
+    EkNN([1,0],  1) = 1 - 1/1 * 0 = 1
+    for k=2:
+    EkNN([-1,0], 1) = 1 - 1/2 * 1 = 1/2
+    EkNN([1,0],  0) = 1 - 1/2 * 1 = 1/2
+    EkNN([-1,0], 0) = 1 - 1/2 * 1 = 1/2
+    EkNN([1,0],  1) = 1 - 1/2 * 1 = 1/2
+
+    energy of the CB is the sum (or average?) of energies it gives to its own cases (without them in the CB)
+    k=1: EkNN() = 1
+    k=2: EkNN() = 1
+
+    """
+    manual_energy_["EnergyKNN"][1] = {
+        "energy_cb": 1,
+        "epsilon": 1e-6,
+        "energy_case_new": [
+            EnergyKNN_energy_1nn(X_, y_)
+            for X_, y_ in zip(X, y)
+        ],
+        "energy_case_from_cb": [
+            1, # EkNN([-1,0], 1) for a CB with only ([1,0],  0) inside
+            1, # EkNN([1,0],  0) for a CB with only ([-1,0], 1) inside
+        ]
+    }
+
+
+    # # CtCoAT
+    # expected_energies = np.array(
+    #     [7.5 + 3*(euclidean_sim(x,X_cb[1])
+    #                if y==1 
+    #                else euclidean_sim(x,X_cb[0])) - 2*(euclidean_sim(x,X_cb[0]) if y==1 else euclidean_sim(x,X_cb[1])) + euclidean_sim(X_cb[0],X_cb[1]) for x,y in zip(X,y)])/27
+    # manual_energy_["CtCoAT"] = {
+        
+    # }
+
+    return [manual_energy_]
---
+++ test/test_EnergyKNN.py
@@ -0,0 +1,79 @@
+import pytest
+from typing import Tuple, List
+import pandas as pd
+import numpy as np
+import torch
+from sklearn.neighbors import KNeighborsClassifier
+
+# load meatcube
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from meatcube2.models import EnergyKNN
+from meatcube2.models.energy_knn_backend import KNNEnergyComputations
+
+
+from scipy.linalg import norm
+from scipy.spatial.distance import euclidean, cosine, correlation, braycurtis, hamming
+def euclidean_sim(x1,x2):
+    return np.exp(-euclidean(x1,x2))
+def cosine_sim(x1,x2):
+    return np.exp(-cosine(x1,x2))
+def correlation_sim(x1,x2):
+    return np.exp(-correlation(x1,x2))
+def hamming_sim(x1,x2):
+    return np.exp(-hamming(x1,x2))
+def radius_sim(x1,x2):
+    return np.exp(-abs(norm(x1)-norm(x2)))
+def class_equality_sim(y1,y2):
+    return np.equal(y1,y2).astype(float)
+
+
+
+def test_manual_energy_cb(manual_energy):
+        # energy of the case
+    for manual_energy_ in manual_energy:
+        ks = manual_energy_["EnergyKNN"].keys()
+        for k in ks:
+            cb = EnergyKNN(euclidean_sim, class_equality_sim, k)
+            cb.fit(manual_energy_["CB X"], manual_energy_["CB y"])
+            calculated_e = cb.energy_cb()
+            e = manual_energy_["EnergyKNN"][k]["energy_cb"]
+            assert abs(calculated_e - e) <= manual_energy_["EnergyKNN"][k]["epsilon"], f'{calculated_e} != {e}'        
+    
+def test_manual_energy_case_new(manual_energy):
+        # energy of the case
+    for manual_energy_ in manual_energy:
+        ks = manual_energy_["EnergyKNN"].keys()
+        for k in ks:
+            cb = EnergyKNN(euclidean_sim, class_equality_sim, k)
+            cb.fit(manual_energy_["CB X"], manual_energy_["CB y"])
+            for X_, y_, e in zip(manual_energy_["X"], manual_energy_["y"], manual_energy_["EnergyKNN"][k]["energy_case_new"]):
+                calculated_e = cb.energy_case_new(X_, y_)
+                assert abs(calculated_e - e) <= manual_energy_["EnergyKNN"][k]["epsilon"], f"error of {calculated_e - e}: |{calculated_e=} - {e=}|"
+        
+def test_manual_1nn_mask(manual_energy):
+    """Check that for the case in the CB, KNNEnergyComputations.knn_mask for k=1 produces a mask with True on the diagonal.
+    
+    This corresponds to checking that each case is its nearest neighbor, which should be true.
+    """
+    for manual_energy_ in manual_energy:
+        cb = EnergyKNN(euclidean_sim, class_equality_sim, 1, precompute_sim_matrix=True)
+        cb.fit(manual_energy_["CB X"], manual_energy_["CB y"])
+        mask = KNNEnergyComputations.knn_mask(cb.X_sim_matrix_, k=1)
+        assert (mask.cpu() == torch.eye(len(cb), dtype=bool)).all(), f"{mask.cpu()} != {torch.eye(len(cb), dtype=bool)}"
+           
+def test_manual_maxnn_mask(manual_energy):
+    """Check if for any input X, KNNEnergyComputations.knn_mask produces a mask full of True when k is the size of the CB"""
+    for manual_energy_ in manual_energy:
+        cb = EnergyKNN(euclidean_sim, class_equality_sim, len(manual_energy_["CB y"]), precompute_sim_matrix=True)
+        cb.fit(manual_energy_["CB X"], manual_energy_["CB y"])
+        
+        # similarity between every case in CB and cases in X
+        sim_S = cb._source_sim_vect(torch.tensor(manual_energy_["X"])).transpose(-1,-2)
+        
+        mask = KNNEnergyComputations.knn_mask(sim_S, k=len(cb))
+        assert (
+            (mask.cpu() == torch.ones((len(cb), len(manual_energy_["X"])), dtype=bool)).all(),
+            f"{mask.cpu()} != { torch.ones((len(cb), len(manual_energy_['X'])), dtype=bool)}"
+        )
+        
